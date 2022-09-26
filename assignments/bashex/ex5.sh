9bd519a6b7bd732da20d5762e893ab4a1914efda
Tyler Gavrity <tgavrity@gmail.com>
2022-09-25 21:22:11 -0700

    pos neg counter

--- assignments/bashex/ex5.sh
+++ assignments/bashex/ex5.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
+read -p "Please enter a number " a;
 
+nums=${a};
+pos=0;
+neg=0;
+for numbers in ${nums}
+do
+	if [ ${numbers} -gt 0 ]
+	then
+		((pos++));
+	else
+		((neg++));
+	fi
+done
+
+echo "Postive Numbers: ${pos}";
+
+echo "Negative Numbers: ${neg}";
